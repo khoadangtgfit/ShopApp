214152130558c5a43edeeb78714b331f34126f71
Đặng Phước Khoa <89892447+khoadangtgfit@users.noreply.github.com>
2024-03-13 21:30:29 +0700

    first commit

---
+++ shopapp-angular/src/main.server.ts
@@ -0,0 +1,6 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { HomeComponent } from './app/home/home.component';
+
+const bootstrap = () => bootstrapApplication(HomeComponent);
+
+export default bootstrap;
